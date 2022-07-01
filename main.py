08ee339c3c462de60e5c033a7f61f6d9e0b7870c
m.afshari <mahdiafshari@aut.ac.ir>
2022-07-01 11:23:05 +0430

    Add initial development

---
+++ main.py
@@ -0,0 +1,9 @@
+# This program works with 128bit charachters knows as ascii
+
+userInput = input("Enter your message: ")
+decimalMessage = []
+for i in userInput:
+    decimalMessage.append(i)
+bytesMessage = []
+# for i in userInput:
+    
